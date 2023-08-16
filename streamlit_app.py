e7b88d7c3de1322c116cc28310bec02e666a1194
Waqar Shahid Qureshi <waqar.shahid@gmail.com>
2023-08-16 10:26:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,7 @@ import torch
 from PIL import Image
 import torchvision.transforms as transforms
 
-# Function to predict the class of an image
-def predict_class(image):
-    # Preprocess the image
+def classify_image(image):
     preprocess = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
@@ -15,24 +13,20 @@ def predict_class(image):
     ])
     input_tensor = preprocess(image).unsqueeze(0)
 
-    # Load a pre-trained model
     model_name = 'resnet34'
     model = timm.create_model(model_name, pretrained=True)
     model.eval()
 
-    # Perform inference
     with torch.no_grad():
         output = model(input_tensor)
         probabilities = torch.nn.functional.softmax(output[0], dim=0)
 
-    # Get the predicted class
     predicted_class_idx = torch.argmax(probabilities).item()
     class_names = timm.data.resolve_data_config({}, model=model)['class_names']
     predicted_class_name = class_names[predicted_class_idx]
 
     return predicted_class_name
 
-# Streamlit app
 st.title("Image Classifier using timm")
 uploaded_file = st.file_uploader("Choose an image from your directory", type=['jpg', 'png', 'jpeg'])
 
@@ -40,5 +34,5 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
     st.write("Classifying...")
-    class_name = predict_class(image)
+    class_name = classify_image(image)
     st.write(f"The predicted class of the image is: {class_name}")
