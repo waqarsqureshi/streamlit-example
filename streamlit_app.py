1ecfa2a98bd03e3e18fa2cddc72bb8af0b62620f
Waqar Shahid Qureshi <waqar.shahid@gmail.com>
2023-08-16 10:24:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,44 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import timm
+import torch
+from PIL import Image
+import torchvision.transforms as transforms
 
-"""
-# Welcome to Streamlit!
+# Function to predict the class of an image
+def predict_class(image):
+    # Preprocess the image
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(image).unsqueeze(0)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    # Load a pre-trained model
+    model_name = 'resnet34'
+    model = timm.create_model(model_name, pretrained=True)
+    model.eval()
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    # Perform inference
+    with torch.no_grad():
+        output = model(input_tensor)
+        probabilities = torch.nn.functional.softmax(output[0], dim=0)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Get the predicted class
+    predicted_class_idx = torch.argmax(probabilities).item()
+    class_names = timm.data.resolve_data_config({}, model=model)['class_names']
+    predicted_class_name = class_names[predicted_class_idx]
 
+    return predicted_class_name
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Streamlit app
+st.title("Image Classifier using timm")
+uploaded_file = st.file_uploader("Choose an image from your directory", type=['jpg', 'png', 'jpeg'])
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.write("Classifying...")
+    class_name = predict_class(image)
+    st.write(f"The predicted class of the image is: {class_name}")
