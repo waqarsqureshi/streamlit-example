ca166c1432daaf556dd6293c0f6a24ce16c2ff5b
Waqar Shahid Qureshi <waqar.shahid@gmail.com>
2023-08-25 16:40:18 +0100

    Add files via upload

--- pavement_utils.py
+++ pavement_utils.py
@@ -18,7 +18,8 @@ import glob
 # import for image segmentation
 from util import get_palette, get_classes
 from mmseg.apis import inference_model, init_model
-
+import zipfile
+import shutil
 #=========================
 from PIL import ImageFont
 
@@ -141,31 +142,9 @@ def classify_image(image_path, model, device):
     top3_probs = top3_prob[0].tolist()  # Save probabilities as floats
     top3_predictions = [(label, prob) for label, prob in zip(top3_labels, top3_probs)]  # Store label and float probability
     predicted_class_name = top3_labels[0]
-    image_with_classes = add_classes_to_image(img, top3_predictions)
-    
-    return predicted_class_name, image_with_classes, top3_predictions 
+    return predicted_class_name, top3_predictions 
 #============================================================
-
-#============================================================
-#The function takes an path by cv2 imread and output the 576x720 image
-def readImage_576x720(path):
-    image = cv2.imread(path) # Read the image using opencv format
-    if image.shape[:2] == (576, 720):
-        orig = np.zeros((576, 720, 3), np.uint8)
-        orig = image
-    elif image.shape[:2] == (542,720):
-        orig = np.zeros((576, 720, 3), np.uint8)
-        orig [34:576,0:720] = image
-    elif image.shape[:2] == (558,720):
-        orig = np.zeros((576, 720, 3), np.uint8)
-        orig [18:576,0:720] = image
-    elif image.shape[:2] == (632,720):
-        orig = np.zeros((576, 720, 3), np.uint8)
-        orig  = image [56:632,0:720]
-    else:
-        orig = cv2.resize(image,(720, 576)) #(w,h) always remember cv2 takes width and height and numpy use height and width
-    return orig
-#==========================================================
+#This function is to process image 1 by 1
 def classify2(folder_path, model, device):
         images = [f for f in glob.glob(os.path.join(folder_path, '**', '*.[pjP][npP][gG]*'), recursive=True)]
         # Sort the image paths
@@ -240,6 +219,60 @@ def classify2(folder_path, model, device):
         else:
             st.error("The zip is not valid and is None")
 #============================================================
+def process_single_image(image_path, model, device):
+    predicted_class_name, top3_predictions = classify_image(image_path, model, device)
+    return predicted_class_name, top3_predictions
+#============================================================
+def update_progress_bar(progress_bar, current_index, total_images):
+    progress_percentage = current_index / total_images
+    progress_bar.progress(progress_percentage)
+#============================================================
+def display_results(images, folder_path):
+    # Dropdown to select an image
+    selected_image_index = st.selectbox("Select an image to view its result:", list(range(len(images))))
+    
+    if selected_image_index in st.session_state.indices:
+        idx = st.session_state.indices.index(selected_image_index)
+        image_name, top3_predictions = st.session_state.results[idx]
+        
+        # Load the image and add classes to it
+        image_path = os.path.join(folder_path, images[idx])
+        image = Image.open(image_path)
+        image_with_classes = add_classes_to_image(image, top3_predictions)
+        
+        st.image(image_with_classes, caption=image_name)
+    else:
+        st.write("Image is not processed yet.")
+
+#============================================================
+def plot_predictions():
+    sorted_indices = [i for i, _ in sorted(enumerate(st.session_state.predictions), key=lambda x: x[1])]
+    sorted_predictions = [st.session_state.predictions[i] for i in sorted_indices]
+    sorted_image_indices = [st.session_state.indices[i] for i in sorted_indices]    
+    
+    plt.scatter(sorted_image_indices, sorted_predictions, marker='o')
+    plt.xlabel('Image Index')
+    plt.ylabel('Prediction')
+    plt.xticks(st.session_state.indices)
+    plt.title('Predictions Over Time')
+    plt.grid(True)
+    st.pyplot(plt)
+#==========================================================
+def save_to_csv(folder_path):
+    if st.button("Save Results to CSV"):
+        timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+        csv_filename = os.path.join(folder_path, f"{timestamp}_results.csv")
+        with open(csv_filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Image Name", "Prediction 1", "Probability 1", "Prediction 2", "Probability 2", "Prediction 3", "Probability 3"])
+            for result in st.session_state.results:
+                image_name, top3 = result
+                row = [image_name] + [item for sublist in top3 for item in sublist]
+                writer.writerow(row)
+        st.success(f"Results saved to {os.path.basename(csv_filename)}")
+        csv_url = f"http://192.168.1.65:8502/{os.path.basename(folder_path)}/{os.path.basename(csv_filename)}"
+        st.markdown(f"[Download CSV File]({csv_url})")
+#==========================================================
 def classify(folder_path, model, device):
     # Check if the folder path is valid
     if folder_path:
@@ -259,23 +292,22 @@ def classify(folder_path, model, device):
         if 'is_running' not in st.session_state:
             st.session_state.is_running = False
 
+        # Create a progress bar
+        progress_bar = st.progress(0)
+
         # Start button to begin processing
         if st.button("Start"):
             st.session_state.is_running = True
+            st.session_state.image_index = 0  # Reset the image index
 
         # Stop button to halt processing
         if st.button("Stop"):
             st.session_state.is_running = False
 
         # If the session is running and there are images left to process
-        if st.session_state.is_running:
-            st.write("Processing the images...")
-
-            # Get the current image path and name
+        while st.session_state.is_running and st.session_state.image_index < len(images):
             image_path = os.path.join(folder_path, images[st.session_state.image_index])
-            
-            # Classify the current image
-            predicted_class_name, _, top3_predictions = classify_image(image_path, model, device)
+            predicted_class_name, top3_predictions = process_single_image(image_path, model, device)
             
             # Update session state with the results
             st.session_state.predictions.append(predicted_class_name)
@@ -284,54 +316,18 @@ def classify(folder_path, model, device):
             
             # Move to the next image
             st.session_state.image_index += 1
-            
-            # If there are more images to process, rerun the script
-            if st.session_state.image_index < len(images):
-                st.experimental_rerun()
-            else:
-                st.session_state.is_running = False
+
+            # Update the progress bar
+            update_progress_bar(progress_bar, st.session_state.image_index, len(images))
+
+        # If all images have been processed or processing is stopped, display the results
+        # If all images are processed or processing is stopped, display the results
+        if not st.session_state.is_running or st.session_state.image_index >= len(images):
+            if st.session_state.image_index >= len(images):
                 st.write("Processing completed!")
-
-        # If all images are processed or processing is stopped, display the graph and results
-        if not st.session_state.is_running:
-            # Dropdown to select an image
-            selected_image_index = st.selectbox("Select an image to view its result:", list(range(len(images))))
-            
-            if selected_image_index in st.session_state.indices:
-                idx = st.session_state.indices.index(selected_image_index)
-                image_name, top3_predictions = st.session_state.results[idx]
-                _, image_with_classes, _ = classify_image(os.path.join(folder_path, images[idx]), model, device)
-                #st.write(os.path.join(folder_path, images[idx]))
-                st.image(image_with_classes, caption=image_name)
-            else:
-                st.write("Image is not processed yet.")
-            
-            # Plot predictions over time
-            sorted_indices = [i for i, _ in sorted(enumerate(st.session_state.predictions), key=lambda x: x[1])]
-            sorted_predictions = [st.session_state.predictions[i] for i in sorted_indices]
-            sorted_image_indices = [st.session_state.indices[i] for i in sorted_indices]    
-            
-            plt.scatter(sorted_image_indices, sorted_predictions, marker='o')
-            plt.xlabel('Image Index')
-            plt.ylabel('Prediction')
-            plt.xticks(st.session_state.indices)
-            plt.title('Predictions Over Time')
-            plt.grid(True)
-            st.pyplot(plt)
-        
-            # Save to CSV button
-            if st.button("Save Results to CSV"):
-                timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-                csv_filename = f"{timestamp}_results.csv"
-                with open(csv_filename, mode='w', newline='') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(["Image Name", "Prediction 1", "Probability 1", "Prediction 2", "Probability 2", "Prediction 3", "Probability 3"])
-                    for result in st.session_state.results:
-                        image_name, top3 = result
-                        row = [image_name] + [item for sublist in top3 for item in sublist]
-                        writer.writerow(row)
-                st.success(f"Results saved to {csv_filename}")
-
+            display_results(images, folder_path)
+            plot_predictions()
+            save_to_csv(folder_path)
     else:
         st.error("The folder path is not valid.")
 
@@ -404,6 +400,25 @@ def initialize_model(model_name, checkpointPath, config=None):
     
     return model, device
 #==============================================================
+#The function takes an path by cv2 imread and output the 576x720 image
+def readImage_576x720(path):
+    image = cv2.imread(path) # Read the image using opencv format
+    if image.shape[:2] == (576, 720):
+        orig = np.zeros((576, 720, 3), np.uint8)
+        orig = image
+    elif image.shape[:2] == (542,720):
+        orig = np.zeros((576, 720, 3), np.uint8)
+        orig [34:576,0:720] = image
+    elif image.shape[:2] == (558,720):
+        orig = np.zeros((576, 720, 3), np.uint8)
+        orig [18:576,0:720] = image
+    elif image.shape[:2] == (632,720):
+        orig = np.zeros((576, 720, 3), np.uint8)
+        orig  = image [56:632,0:720]
+    else:
+        orig = cv2.resize(image,(720, 576)) #(w,h) always remember cv2 takes width and height and numpy use height and width
+    return orig
+#==========================================================
 def result_segImage(result,palette=None,CLASS_NAMES=None):
         """compute`resulted mask` for the `img`.
 
@@ -442,27 +457,31 @@ def result_segImage(result,palette=None,CLASS_NAMES=None):
 
         return color_seg
 #==============================================================
-def process_image(path, model):
-    orig = readImage_576x720(path)
+def process_image(image_path, model):
+    orig = readImage_576x720(image_path)
     result = inference_model(model, orig)
-    result2 = (result.pred_sem_seg.data[0])
+    result2 = result.pred_sem_seg.data[0]
     mask = result_segImage(result2, palette=get_palette('roadsurvey'), CLASS_NAMES=get_classes('roadsurvey'))
     segImage = cv2.bitwise_and(orig, mask)
     return segImage, orig
 #=============================================================
-def should_discard_image(segImage, orig):
+def should_discard_image(segImage, orig, threshold_percentage):
     gray_mask = cv2.cvtColor(segImage, cv2.COLOR_BGR2GRAY)
     count = cv2.countNonZero(gray_mask)
     contours, _ = cv2.findContours(gray_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    thres = (orig.shape[0] * orig.shape[1]) * 0.10
+    thres = (orig.shape[0] * orig.shape[1]) * threshold_percentage
     return count < thres or len(contours) > 25
 #=============================================================
-def PavementExtraction(folder_path, model, device):
+#This function is used when you like to see the segmentation 1 by 1
+def PavementExtraction2(folder_path, model, device):
     if folder_path:
         if os.path.isdir(folder_path):
             images = [f for f in glob.glob(os.path.join(folder_path, '**', '*.[pjP][npP][gG]*'), recursive=True)]
             # Sort the image paths
             images.sort()
+            # Allow the user to set the threshold value
+            threshold_percentage = st.slider("Set the threshold percentage:", min_value=0.0, max_value=1.0, value=0.10, step=0.01)
+
             image_names = [os.path.basename(image) for image in images]  # Extracting only the image names
             selected_image_name = st.selectbox("Choose an image:", image_names)
             selected_image_index = image_names.index(selected_image_name)
@@ -470,7 +489,7 @@ def PavementExtraction(folder_path, model, device):
             segImage, orig = process_image(image_path, model)
             
             # Check if the image should be discarded
-            if not should_discard_image(segImage, orig):
+            if not should_discard_image(segImage, orig,threshold_percentage=0.10):
                 # Display the segmented images in Streamlit
                 segImage_resized = cv2.resize(segImage, (700, 300))
                 st.image(segImage_resized, caption="Pavement Extracted Image", use_column_width=True)
@@ -488,8 +507,75 @@ def PavementExtraction(folder_path, model, device):
                         st.error("Please enter a valid folder path.")
             else:
                 st.warning("The image was discarded due to poor segmentation results.")
+####################################################
+def zip_folder(folder_path, zip_name):
+    with zipfile.ZipFile(zip_name, 'w') as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), folder_path))
+##################################################
+def process_all_images(folder_path, model, save_folder,threshold_percentage):
+    # Create a directory for saving processed images
+    os.makedirs(save_folder, exist_ok=True)
+    
+    images = [f for f in glob.glob(os.path.join(folder_path, '**', '*.[pjP][npP][gG]*'), recursive=True)]
+    total_images = len(images)
+    
+    # Create a progress bar in Streamlit
+    progress_bar = st.progress(0)
+    
+    for image_path in images:
+        # Check if the processing should be stopped
+        if 'stop_processing' in st.session_state and st.session_state.stop_processing:
+            break
+        
+        segImage, orig = process_image(image_path, model)
+        if not should_discard_image(segImage, orig, threshold_percentage):
+            segImg_path = os.path.join(save_folder, f"{os.path.splitext(os.path.basename(image_path))[0]}_segImg{os.path.splitext(image_path)[1]}")
+            cropImg_path = os.path.join(save_folder, f"{os.path.splitext(os.path.basename(image_path))[0]}_cropImg{os.path.splitext(image_path)[1]}")
+            save(segImg_path, cropImg_path, image_path, None, segImage[230:560,0:700], orig[230:560,0:700])
 
+        # Update the current image index in session state
+        st.session_state.current_image_index += 1
+        
+        # Update the progress bar
+        progress_bar.progress(st.session_state.current_image_index / total_images)
 
+    # If all images have been processed, zip the folder
+    if st.session_state.current_image_index >= total_images:
+        zip_name = os.path.join(folder_path, "processed_images.zip")
+        zip_folder(save_folder, zip_name)
+        st.write(os.path.basename(folder_path))
+        zip_url = f"http://192.168.1.65:8502/{os.path.basename(folder_path)}/{os.path.basename(zip_name)}"
+        st.markdown(f"[Download Processed Images]({zip_url})")
+        progress_bar.progress(0)
+#################################################
+def save(segImg_path, cropImg_path, path, args, segImage, orig):
+    # Assuming this function saves the segmented and cropped images
+    cv2.imwrite(segImg_path, segImage)
+    cv2.imwrite(cropImg_path, orig)
+#######################################################
+def PavementExtraction(folder_path, model, device):
+    if folder_path:
+        if os.path.isdir(folder_path):
+            # Allow the user to set the threshold value
+            threshold_percentage = st.slider("Set the threshold percentage:", min_value=0.0, max_value=1.0, value=0.10, step=0.01)
 
+            # Initialize session state variables if they don't exist
+            if 'current_image_index' not in st.session_state:
+                st.session_state.current_image_index = 0
+            if 'stop_processing' not in st.session_state:
+                st.session_state.stop_processing = False
 
+            # Start button to begin processing
+            if st.button("Start"):
+                st.session_state.stop_processing = False
+                save_folder = "processed_images"
+                process_all_images(folder_path, model, save_folder,threshold_percentage)
+
+            # Stop button to halt processing
+            if st.button("Stop"):
+                st.session_state.stop_processing = True
+        else:
+            st.error("The folder path is not valid.")
   
